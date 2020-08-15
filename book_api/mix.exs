9cf2686500c15a918f225c9818de1ca597b0824d
japerry911 <jack.e.perry@outlook.com>
2020-08-15 12:01:49 -0600

    documentation added to book api

--- book_api/mix.exs
+++ book_api/mix.exs
@@ -42,7 +42,8 @@ defmodule BookApi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 
